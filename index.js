10b7f05a80920924695d07cf9ae94bfd20db7ed1
MinwooPark93 <79454069+MinwooPark93@users.noreply.github.com>
2022-03-11 12:51:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// 내 풀이 방법
+
+function solution(num) {
+    var answer = '';
+    
+    if (num % 2 == 0) {
+        answer = 'Even';
+    }
+    else {
+        answer = 'Odd';
+    }
+    return answer;
+}
+
+console.log(solution(1));
+
+
+// 상위권 랭크 풀이 방법
+
+function evenOrOdd(num) {
+  return num % 2 ? "Odd" : "Even";
+}
+
+console.log(evenOrOdd(2));
+
+
+// 위랑 비슷한 식이지만 괄호 사용
+
+function oddOrEven(num) {
+  return (num % 2)? "Odd":"Even";
+}
+
+console.log(oddOrEven(3))
+
+// 삼항연산자에 대해서 공부해 볼 필요가 있을 것 같음
